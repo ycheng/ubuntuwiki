aa8b4f4ca3d9de9696b55d75f38ba5c634b239da
Y.C Cheng <yc.cheng@canonical.com>
2013-11-25 14:48:10 +0800

    initial dump (touch part) from wiki.ubuntu.com at Mon, 25 Nov 2013 14:47:34 +0800

---
+++ bin/dump.sh
@@ -0,0 +1,13 @@
+export P=https://wiki.ubuntu.com
+editmoin -d Touch.wiki $P/Touch/
+editmoin -d Touch/Install.wiki $P/Touch/Install.wiki
+editmoin -d Touch/Porting.wiki $P/Touch/Porting
+editmoin -d Touch/ReleaseNotes.wiki $P/Touch/ReleaseNotes
+editmoin -d Touch/SdkFeatureStatus.wiki $P/Touch/SdkFeatureStatus
+editmoin -d Touch/CoreApps.wiki $P/Touch/CoreApps
+editmoin -d Touch/Devices.wiki $P/Touch/Devices
+editmoin -d Touch/Porting.wiki $P/Touch/Porting
+editmoin -d Touch/Building.wiki $P/Touch/Building
+editmoin -d Touch/Testing.wiki $P/Touch/Testing
+#editmoin -d Touch/.wiki $P/Touch/
+
