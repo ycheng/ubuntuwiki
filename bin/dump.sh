8483d4e3a927814ed99a8f17f817764914107aa0
Y.C Cheng <yc.cheng@canonical.com>
2013-11-26 10:53:09 +0800

    dump.sh: sort node name, and update page content

--- bin/dump.sh
+++ bin/dump.sh
@@ -1,13 +1,11 @@
 export P=https://wiki.ubuntu.com
-editmoin -d Touch.wiki $P/Touch/
-editmoin -d Touch/Install.wiki $P/Touch/Install.wiki
-editmoin -d Touch/Porting.wiki $P/Touch/Porting
-editmoin -d Touch/ReleaseNotes.wiki $P/Touch/ReleaseNotes
-editmoin -d Touch/SdkFeatureStatus.wiki $P/Touch/SdkFeatureStatus
+editmoin -d Touch/Building.wiki $P/Touch/Building
 editmoin -d Touch/CoreApps.wiki $P/Touch/CoreApps
 editmoin -d Touch/Devices.wiki $P/Touch/Devices
-editmoin -d Touch/Porting.wiki $P/Touch/Porting
-editmoin -d Touch/Building.wiki $P/Touch/Building
+editmoin -d Touch/Install.wiki $P/Touch/Install
+editmoin -d Touch/PortingNewTree.wiki $P/Touch/PortingNewTree
+editmoin -d Touch/ReleaseNotes.wiki $P/Touch/ReleaseNotes
+editmoin -d Touch/SdkFeatureStatus.wiki $P/Touch/SdkFeatureStatus
 editmoin -d Touch/Testing.wiki $P/Touch/Testing
+editmoin -d Touch.wiki $P/Touch/
 #editmoin -d Touch/.wiki $P/Touch/
-
