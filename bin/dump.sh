42f1bc552b3a2ee005aa78f096e66f69f793747d
Yuan-Chen Cheng <yc.cheng@canonical.com>
2014-03-18 07:57:34 +0800

    Tue, 18 Mar 2014 07:57:34 +0800

--- bin/dump.sh
+++ bin/dump.sh
@@ -1,16 +1,17 @@
 export P=https://wiki.ubuntu.com
-editmoin -d Touch/AOSPBuild.wiki $P/Touch/AOSPBuild
-editmoin -d Touch/Building.wiki $P/Touch/Building
-editmoin -d Touch/Contribute.wiki $P/Touch/Contribute
-editmoin -d Touch/CoreApps.wiki $P/Touch/CoreApps
-editmoin -d Touch/Devices.wiki $P/Touch/Devices
-editmoin -d Touch/DualBootInstallation.wiki $P/Touch/DualBootInstallation
-editmoin -d Touch/Gerrit.wiki $P/Touch/Gerrit
-editmoin -d Touch/Install.wiki $P/Touch/Install
-editmoin -d Touch/Porting.wiki $P/Touch/Porting
-editmoin -d Touch/PortingNewTree.wiki $P/Touch/PortingNewTree
-editmoin -d Touch/ReleaseNotes.wiki $P/Touch/ReleaseNotes
-editmoin -d Touch/SdkFeatureStatus.wiki $P/Touch/SdkFeatureStatus
-editmoin -d Touch/Testing.wiki $P/Touch/Testing
-editmoin -d Touch.wiki $P/Touch/
+editmoin -d Click/Frameworks.wiki $P/Click/Frameworks ; sleep 5
+editmoin -d Touch/AOSPBuild.wiki $P/Touch/AOSPBuild ; sleep 5
+editmoin -d Touch/Building.wiki $P/Touch/Building ; sleep 5
+editmoin -d Touch/Contribute.wiki $P/Touch/Contribute ; sleep 5
+editmoin -d Touch/CoreApps.wiki $P/Touch/CoreApps ; sleep 5
+editmoin -d Touch/Devices.wiki $P/Touch/Devices ; sleep 5
+editmoin -d Touch/DualBootInstallation.wiki $P/Touch/DualBootInstallation ; sleep 5
+editmoin -d Touch/Gerrit.wiki $P/Touch/Gerrit ; sleep 5
+editmoin -d Touch/Install.wiki $P/Touch/Install ; sleep 5
+editmoin -d Touch/Porting.wiki $P/Touch/Porting ; sleep 5
+editmoin -d Touch/PortingNewTree.wiki $P/Touch/PortingNewTree ; sleep 5
+editmoin -d Touch/ReleaseNotes.wiki $P/Touch/ReleaseNotes ; sleep 5
+editmoin -d Touch/SdkFeatureStatus.wiki $P/Touch/SdkFeatureStatus ; sleep 5
+editmoin -d Touch/Testing.wiki $P/Touch/Testing ; sleep 5
+editmoin -d Touch.wiki $P/Touch/ ; sleep 5
 #editmoin -d Touch/.wiki $P/Touch/
