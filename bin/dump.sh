57258c51bdb3d500cd6c485520c1d895ed3a1f04
Yuan-Chen Cheng <yc.cheng@canonical.com>
2014-05-06 21:45:55 +0800

    Tue, 06 May 2014 21:45:55 +0800

--- bin/dump.sh
+++ bin/dump.sh
@@ -1,21 +1,23 @@
 export P=https://wiki.ubuntu.com
-editmoin -d Click/Frameworks.wiki $P/Click/Frameworks ; sleep 5
-editmoin -d Touch/AdbNetworking.wiki $P/Touch/AdbNetworking ; sleep 5
-editmoin -d Touch/AOSPBuild.wiki $P/Touch/AOSPBuild ; sleep 5
-editmoin -d Touch/Building.wiki $P/Touch/Building ; sleep 5
-editmoin -d Touch/Contribute.wiki $P/Touch/Contribute ; sleep 5
-editmoin -d Touch/CoreApps.wiki $P/Touch/CoreApps ; sleep 5
-editmoin -d Touch/DailyDriver.wiki $P/Touch/DailyDriver ; sleep 5
-editmoin -d Touch/Devices.wiki $P/Touch/Devices ; sleep 5
-editmoin -d Touch/DualBootInstallation.wiki $P/Touch/DualBootInstallation ; sleep 5
-editmoin -d Touch/Emulator.wiki $P/Touch/Emulator ; sleep 5
-editmoin -d Touch/Gerrit.wiki $P/Touch/Gerrit ; sleep 5
-editmoin -d Touch/Install.wiki $P/Touch/Install ; sleep 5
-editmoin -d Touch/MakingPortsAvailable.wiki $P/Touch/MakingPortsAvailable.wiki ; sleep 5
-editmoin -d Touch/Porting.wiki $P/Touch/Porting ; sleep 5
-editmoin -d Touch/PortingNewTree.wiki $P/Touch/PortingNewTree ; sleep 5
-editmoin -d Touch/ReleaseNotes.wiki $P/Touch/ReleaseNotes ; sleep 5
-editmoin -d Touch/SdkFeatureStatus.wiki $P/Touch/SdkFeatureStatus ; sleep 5
-editmoin -d Touch/Testing.wiki $P/Touch/Testing ; sleep 5
-editmoin -d Touch.wiki $P/Touch/ ; sleep 5
+bin/editmoin -d Click/Frameworks.wiki $P/Click/Frameworks ; sleep 5
+bin/editmoin -d Touch/AdbNetworking.wiki $P/Touch/AdbNetworking ; sleep 5
+bin/editmoin -d Touch/AOSPBuild.wiki $P/Touch/AOSPBuild ; sleep 5
+bin/editmoin -d Touch/Building.wiki $P/Touch/Building ; sleep 5
+bin/editmoin -d Touch/Contribute.wiki $P/Touch/Contribute ; sleep 5
+bin/editmoin -d Touch/CoreApps.wiki $P/Touch/CoreApps ; sleep 5
+bin/editmoin -d Touch/DailyDriver.wiki $P/Touch/DailyDriver ; sleep 5
+bin/editmoin -d Touch/Devices.wiki $P/Touch/Devices ; sleep 5
+bin/editmoin -d Touch/DualBootInstallation.wiki $P/Touch/DualBootInstallation ; sleep 5
+sleep 120
+bin/editmoin -d Touch/Emulator.wiki $P/Touch/Emulator ; sleep 5
+bin/editmoin -d Touch/Gerrit.wiki $P/Touch/Gerrit ; sleep 5
+bin/editmoin -d Touch/Install.wiki $P/Touch/Install ; sleep 5
+bin/editmoin -d Touch/MakingPortsAvailable.wiki $P/Touch/MakingPortsAvailable.wiki ; sleep 5
+
+bin/editmoin -d Touch/Porting.wiki $P/Touch/Porting ; sleep 5
+bin/editmoin -d Touch/PortingNewTree.wiki $P/Touch/PortingNewTree ; sleep 5
+bin/editmoin -d Touch/ReleaseNotes.wiki $P/Touch/ReleaseNotes ; sleep 5
+bin/editmoin -d Touch/SdkFeatureStatus.wiki $P/Touch/SdkFeatureStatus ; sleep 5
+bin/editmoin -d Touch/Testing.wiki $P/Touch/Testing ; sleep 5
+bin/editmoin -d Touch.wiki $P/Touch/ ; sleep 5
 #editmoin -d Touch/.wiki $P/Touch/
